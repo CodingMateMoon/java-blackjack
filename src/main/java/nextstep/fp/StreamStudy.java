9a62fa92b38771ed12cf35de17b245249689a35b
codingmatemoon <abc9529@naver.com>
2022-08-23 00:16:50 +0900

    stream filter, distinct, map, sorted((a,b)->b.length - a.length), forEach 등 활용

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -28,6 +29,7 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream().filter(word->word.length() > 12).distinct().map(w->w.toLowerCase()).sorted((a,b)->b.length() - a.length()).forEach(s-> System.out.println(s));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +41,6 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number->number > 3).mapToInt(greaterThanThree -> greaterThanThree * 2).sum();
     }
 }
